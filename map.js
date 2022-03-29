0a128f8e06352f17e80b5ede9921a49e822038db
g1moon <gmoon.cho@gmail.com>
2022-03-29 14:44:49 +0900

    feat: Array.prototype.map()과 유사한 map 추가   - feat: products 예시 object 추가   - feat: 만들어진 map을 통해 product의 name만 추출해 출력 추가

---
+++ map.js
@@ -0,0 +1,21 @@
+// f는 (item) => item.name 이런 형태
+const map = (f, iter) => {
+  const res = [];
+
+  for (const i of iter) {
+    res.push(f(i));
+  }
+
+  return res;
+};
+
+const products = [
+  {name: '반팔티', price: 15000},
+  {name: '긴팔티', price: 20000},
+  {name: '핸드폰케이스', price: 15000},
+  {name: '후드티', price: 30000},
+  {name: '바지', price: 25000}
+];
+
+console.log(map(product => product.name, products));
+
