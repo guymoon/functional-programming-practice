9bb9c4c8c55c81c1d827eb618772c4046d9eaa0b
g1moon <gmoon.cho@gmail.com>
2022-03-30 14:00:37 +0900

    feat: go(즉시 한숨들과 인자들을 전달해 평가하는 함수) 추가   - feat: go로 25000원 아래 상품들의 총 가격을 계산해 출력하는 부분 추가

---
+++ go_pipe.js
@@ -0,0 +1,45 @@
+const go = (...args) => reduce((acc, f) => f(acc), args);
+
+const goResult = go(0,
+  a => a + 1,
+  a => a + 10,
+  a => a + 20
+);
+
+console.log(goResult); // 31
+
+const products = [
+  {name: '반팔티', price: 15000},
+  {name: '긴팔티', price: 20000},
+  {name: '핸드폰케이스', price: 15000},
+  {name: '후드티', price: 30000},
+  {name: '바지', price: 25000}
+];
+
+// go로 가독성 증가시키기
+const totalPriceUnder25000Ver1 = go(
+  products,
+  products => filter(product => product.price <= 25000, products),
+  productsUnder25000 => map(product => product.price, productsUnder25000),
+  pricesUnder25000 => reduce((acc, cur) => acc + cur, pricesUnder25000)
+);
+
+const totalPriceUnder25000Ver2 = go(
+  products,
+  products => filter(product => product.price <= 25000, products),
+  productsUnder25000 => map(product => product.price, productsUnder25000),
+  pricesUnder25000 => reduce((acc, cur) => acc + cur, pricesUnder25000)
+);
+
+const totalPriceUnder25000Ver3 = go(
+  products,
+  filter(product => product.price <= 25000),
+  map(product => product.price),
+  reduce((acc, cur) => acc + cur)
+);
+
+console.log(totalPriceUnder25000Ver1); // 75000
+console.log(totalPriceUnder25000Ver2); // 75000
+console.log(totalPriceUnder25000Ver3); // 75000
+
+
