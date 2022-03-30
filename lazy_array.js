ea43795fc433bd3f8d84866e2d17bba940e3afc2
g1moon <gmoon.cho@gmail.com>
2022-03-30 14:05:51 +0900

    feat: lazy map, filter, range 추가   - feat: lazy map(한번에 모든 배열을 생산하지 않으면서 특정 요소를 뽑아 새로운 배열을 리턴하는 함수) 추가   - feat: lazy filter(한번에 모든 배열을 생산하지 않으면서 조건에 맞는 것들을 뽑아 리턴하는 함수) 추가   - feat: lazy range(한번에 모든 배열을 생산하지 않으면서 범위에 따른 배열을 리턴하는 함수) 추가

---
+++ lazy_array.js
@@ -0,0 +1,34 @@
+import {curry} from './curry'
+
+const L = {};
+
+// range
+L.range = function* (limit) {
+  let i = 0;
+  while (i < limit) {
+    yield i++;
+  }
+}
+
+const iter = L.range(4);
+console.log(...iter) // 0, 1, 2, 3
+console.log(iter); // Generator
+console.log(reduce(add, iter)); // 6
+
+// map
+L.map = function* (f, iter) {
+  for (const i of iter) yield f(i);
+};
+
+const lazyMapResult = L.map(num => num + 1, [0, 0, 0]);
+console.log(...lazyMapResult);
+
+// filter
+L.filter = function* (f, iter) {
+  for (const i of iter) {
+    if(f(i)) yield i;
+  };
+};
+
+const lazyFilterResult = L.filter(num => num > 5, [0, 10, 15]);
+console.log(...lazyFilterResult);
