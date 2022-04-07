e2fd1f685f5e7de5953027853e64461c87e22b1d
g1moon <gmoon.cho@gmail.com>
2022-04-08 00:18:05 +0900

    feat: queryStr 추가(문자열을 합치는 join과 비슷한 역할)

--- lazy_array.js
+++ lazy_array.js
@@ -1,4 +1,6 @@
 import {curry} from './curry'
+import {reduce} from './map_filter_reduce';
+import {map, pipe} from './fx';
 
 const L = {};
 
@@ -45,4 +47,25 @@ console.log(join('-', function *() {
   yield 2;
   yield 3;
 }())); // 1-2-3
+
+const queryStr = pipe(
+  L.entries,
+  L.map(([key, value]) => `${key}=${value}`),
+  join('-'),
+);
+
+// // 이것도 인자를 전달하고 있으니 잘 동작
+// // const queryStr = pipe(
+// //   Object.entries,
+// //   map(([key, value]) => `${key}=${value}`),
+// //   join('-'),
+// // );
+//
+// const queryStr = pipe(
+//   L.entries,
+//   map(([key, value]) => `${key}=${value}`),
+//   join('-'),
+// );
+
+console.log(queryStr({name: '기문', type: 'student', nickname: '끼문'}))
 
